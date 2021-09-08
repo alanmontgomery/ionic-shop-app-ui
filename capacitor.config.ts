9da786b59982ddade693511902be3bb4a72c5f92
alanmontgomery <alan.montgomery@tascomi.com>
2021-09-08 20:49:17 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'ionic-shop-app-ui',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
